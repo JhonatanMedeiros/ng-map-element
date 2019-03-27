0bd6e4719e59645eff1574dc02343362fe6feda0
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2019-03-26 23:53:25 -0300

    Create script deploy-gh-pages.js

---
+++ deploy-gh-pages.js
@@ -0,0 +1,12 @@
+const ghpages = require('gh-pages');
+
+const GH_TOKEN = process.env.GH_TOKEN || 'TOKEN';
+const GH_EMAIL = process.env.GH_EMAIL || 'EMAIL';
+
+ghpages.publish('demo', {
+  branch: 'gh-pages',
+  remote: 'origin',
+  repo: `https://${GH_TOKEN}@github.com/JhonatanMedeiros/ng-map-element.git`,
+  silent: false,
+  user: { name: 'Jhonatan Medeiros', email: GH_EMAIL}
+});
